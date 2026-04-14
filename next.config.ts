96f92194263157ab2bc6be925323c53859bc5903
Paulo <paulo.btx@gmail.com>
2026-04-14 12:18:52 -0300

    Fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: '/prevenindocomarte',
+  assetPrefix: '/prevenindocomarte',
   images: {
     unoptimized: true,
   },
